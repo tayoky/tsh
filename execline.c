5929723dd8ddfd38c9afd35c23230fed7836c55a
tayoky <tayoky4848@gmail.com>
2025-03-13 08:40:44 +0100

    add string support

--- execline.c
+++ execline.c
@@ -33,28 +33,40 @@ struct cmd builtin[]= {
 
 char **parse_line(char *line,int *out){
 	char prev_was_space = 1;
+	char in_string = 0;
 	int argc = 0;
 	char **argv = malloc(1);
+	size_t arg_size = 0;
 
 	for(int i=0;line[i];i++){
 		if(line[i] == '\n'){
 			line[i] = '\0';
 			break;
 		}
-		if(line[i] == ' '){
+		if(line[i] == ' ' && !in_string){
 			line[i] = '\0';
 			prev_was_space = 1;
 			continue;
 		}
 
-
 		//check if we just start a new arg
 		if(prev_was_space){
 			prev_was_space = 0;
 			argv = realloc(argv,(argc + 1) * sizeof(char *));
-			argv[argc] = &line[i];
+			argv[argc] = malloc(1);
 			argc++;
+			arg_size = 0;
 		}
+
+		if(line[i] == '"'){
+			in_string = 1-in_string;
+			continue;
+		}
+
+		argv[argc-1][arg_size] = line[i];
+		arg_size++;
+		argv[argc-1] = realloc(argv[argc-1],arg_size + 1);
+		argv[argc-1][arg_size] = '\0';
 	}
 
 	//add the NULL at the end
---
+++ test/string.sh
@@ -0,0 +1,5 @@
+#!/bin/tsh
+
+#string test
+echo "string with     spaces"
+"echo" "to     files" > "string_file.txt"
