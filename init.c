022fac0d20acf553ee9d3dec9f0fa28aa76a1cb0
tayoky <tayoky4848@gmail.com>
2025-04-27 21:40:01 +0200

    add argument support for scripts

--- init.c
+++ init.c
@@ -6,8 +6,21 @@
 
 //initialize the environement
 void init(int argc,char **argv){
+	//setup $TSH
 	char *tsh = malloc(strlen(argv[0]) + 5);
 	sprintf(tsh,"TSH=%s",argv[0]);
 	putenv(tsh);
 	free(tsh);
+	
+	//setup argument
+	//for use in scripts
+	int n = 0;
+	for(int i=1;i<argc;i++){
+		if(argv[i][0] == '-')continue;
+		char *arg = malloc(strlen(argv[i]) + 64);
+		sprintf(arg,"%d=%s",n,argv[i]);
+		putenv(arg);
+		free(arg);
+		n++;
+	}
 }
