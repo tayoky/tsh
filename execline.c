e2e61e11158953f698bf263de90f21a2d3c2338f
tayoky <tayoky4848@gmail.com>
2025-06-15 22:28:26 +0200

    interpreter : very basic interpreter

--- execline.c
+++ execline.c
@@ -2,12 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include "tsh.h"
 #include "malloc-check.h"
 
 int lock = 0;
+int exit_code;
+
+int launch(char *const*args){
+	pid_t child = fork();
+	if(!child){
+		execvp(args[0],args);
+		error("%s : %s",args[0],strerror(errno));
+		exit(1);
+	}
+	if(child < 0){
+		error("fork : %s",strerror(errno));
+		return 1;
+	}
+	int status;
+	if(waitpid(child,&status,0) < 0){
+		error("waitpid : %s",strerror(errno));
+		return 1;
+	}
+	
+	exit_code = status;
+	return status;
+}
+
+int execute_node(AST_node *node){
+	switch(node->type){
+	case AST_EXPR:
+		return execute_node(node->right);
+	case AST_AND:;
+		int and_status = execute_node(node->left);
+		if(and_status)return and_status;
+		return execute_node(node->right);
+	case AST_OR:;
+		int or_status = execute_node(node->left);
+		if(!or_status)return or_status;
+		return execute_node(node->right);
+	case AST_COMMAND:;
+		int argc = 0;
+		for(AST_node *cur = node->left;cur;cur = cur->right){
+			argc++;
+		}
+		char **argv = malloc(sizeof(char *) * (argc + 1));
+		argv[argc] = NULL;
+		int i=0;
+		for(AST_node *cur = node->left;cur;cur = cur->right){
+			argv[i] = cur->value;
+			i++;
+		}
+
+		int status = launch(argv);
+		free(argv);
+		return status;
+	}
+	return 0;
+}
 
 
 void print_node(AST_node *node,int depth){
@@ -56,6 +111,8 @@ int exec_line(char *line){
 		goto tokens_cleanup;
 	}
 	print_node(root,0);
+
+	execute_node(root);
 
 ast_cleanup:
 	ast_cleanup(root);
